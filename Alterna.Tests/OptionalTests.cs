e75e62c103d0c4a68e8c4ed06f69a94660afc092
Botond Balazs <balazsbotond@gmail.com>
2016-12-30 09:23:03 +0100

    Copy WIP implementation and tests into the solution

---
+++ Alterna.Tests/OptionalTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Alterna.Tests
+{
+    public class OptionalTests
+    {
+        [Fact]
+        public void NoneHasNoValue()
+        {
+            Optional<string>.None.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void NoneValueThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var v = Optional<string>.None.Value;
+            });
+        }
+
+        [Fact]
+        public void SomeHasValue()
+        {
+            Optional<string>.Some("a").HasValue.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SomeHasCorrectValue()
+        {
+            Optional<string>.Some("a").Value.Should().Be("a");
+        }
+
+        [Fact]
+        public void IfSomeNotExecutedIfNone()
+        {
+            Optional<string>.None.Match(ifSome: a => { throw new Exception(); });
+        }
+
+        [Fact]
+        public void IfNoneNotExecutedIfSome()
+        {
+            Optional<string>.Some("a").Match(ifNone: () => { throw new Exception(); });
+        }
+
+        [Fact]
+        public void IfSomeExecutedIfSome()
+        {
+            var e = false;
+            Optional<string>.Some("a").Match(
+                ifSome: a => e = true,
+                ifNone: () => { throw new Exception(); });
+
+            e.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IfNoneExecutedIfNone()
+        {
+            var e = false;
+            Optional<string>.None.Match(
+                ifSome: a => { throw new Exception(); },
+                ifNone: () => e = true);
+
+            e.Should().BeTrue();
+        }
+
+        [Fact]
+        public void FromReturnsNoneIfGivenNull()
+        {
+            Optional<string>.From(null).HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void FromReturnsSomeIfGivenNonNull()
+        {
+            Optional<string>.From("a").HasValue.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueOrDefaultReturnsValueIfOptionalHasValue()
+        {
+            Optional<string>.Some("a").ValueOrDefault().Should().Be("a");
+        }
+
+        [Fact]
+        public void ValueOrDefaultReturnsTypeDefaultValueIfOptionalHasNoValueAndNoCustomDefaultSupplied()
+        {
+            Optional<int>.None.ValueOrDefault().Should().Be(0);
+        }
+
+        [Fact]
+        public void ValueOrDefaultReturnsCustomDefaultValueIfSpecifiedAndOptionalHasNoValue()
+        {
+            Optional<int>.None.ValueOrDefault(42).Should().Be(42);
+        }
+    }
+}
---
+++ Alterna/OptionalOfT.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Alterna
+{
+    public struct Optional<T> : IEquatable<Optional<T>>
+    {
+        public static Optional<T> None => default(Optional<T>);
+        public static Optional<T> Some(T value) => new Optional<T>(value);
+        public static Optional<T> From(T value) =>
+            value == null ? None : Some(value);
+
+        public bool HasValue { get; }
+
+        private readonly T value;
+        public T Value
+        {
+            get
+            {
+                if (!HasValue)
+                    throw new InvalidOperationException();
+
+                return value;
+            }
+        }
+
+        private Optional(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            this.value = value;
+            HasValue = true;
+        }
+
+        public void Match(Action<T> ifSome = null, Action ifNone = null)
+        {
+            if (ifSome != null && HasValue)
+                ifSome(Value);
+            if (ifNone != null && !HasValue)
+                ifNone();
+        }
+
+        public U Convert<U>(Func<T, U> ifSome, Func<U> ifNone) { throw new NotImplementedException(); }
+
+        public void Map<U>(Func<T, Optional<U>> mapper) { }
+
+        public Optional<U> FlatMap<U>(Func<T, Optional<U>> mapper) { throw new NotImplementedException(); }
+
+        public Optional<T> When(Predicate<T> condition) { throw new NotImplementedException(); }
+
+        public T ValueOrDefault(T defaultValue = default(T)) =>
+            HasValue ? Value : defaultValue;
+
+        public bool Equals(Optional<T> other) =>
+            Value.Equals(other.Value) && HasValue == other.HasValue;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Optional<T>)
+                return Equals((Optional<T>)obj);
+
+            return false;
+        }
+
+        public override int GetHashCode() =>
+            Value.GetHashCode() ^ HasValue.GetHashCode();
+    }
+}
