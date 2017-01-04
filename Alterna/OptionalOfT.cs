6da011df41dad73d55fed64a8b89fc927c52a531
Botond Balazs <balazsbotond@gmail.com>
2017-01-04 11:51:14 +0100

    Add more docs; Implement ToString

--- Alterna/OptionalOfT.cs
+++ Alterna/OptionalOfT.cs
@@ -7,7 +7,7 @@ namespace Alterna
     ///     Represents a value that may or may not be present.
     /// </summary>
     /// <typeparam name="T">
-    ///     The underlying type of the <c>Optional</c>
+    ///     The underlying type of the <c>Optional</c>.
     /// </typeparam>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public struct Optional<T> : IEquatable<Optional<T>>
@@ -21,13 +21,13 @@ namespace Alterna
         ///     Returns an <c>Optional</c> with the specified non-null value.
         /// </summary>
         /// <param name="value">
-        ///     The non-null value to wrap in an <c>Optional</c>
+        ///     The non-null value to wrap in an <c>Optional</c>.
         /// </param>
         /// <returns>
-        ///     An <c>Optional</c> with the value specified
+        ///     An <c>Optional</c> with the value specified.
         /// </returns>
         /// <exception cref="InvalidOperationException">
-        ///     If <paramref name="value"/> is <c>null</c>
+        ///     If <paramref name="value"/> is <c>null</c>.
         /// </exception>
         public static Optional<T> Some(T value) => new Optional<T>(value);
 
@@ -36,11 +36,11 @@ namespace Alterna
         ///     non-null, otherwise returns an empty <c>Optional</c>.
         /// </summary>
         /// <param name="value">
-        ///     The possibly-null value to describe
+        ///     The possibly-null value to describe.
         /// </param>
         /// <returns>
         ///     An <c>Optional</c> with the value specified if it is non-null,
-        ///     otherwise an empty <c>Optional</c>
+        ///     otherwise an empty <c>Optional</c>.
         /// </returns>
         public static Optional<T> From(T value) =>
             value == null ? None : Some(value);
@@ -57,7 +57,7 @@ namespace Alterna
         ///     throws an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <exception cref="InvalidOperationException">
-        ///     If the <c>Optional</c> has no value
+        ///     If the <c>Optional</c> has no value.
         /// </exception>
         public T Value
         {
@@ -79,14 +79,14 @@ namespace Alterna
         ///     At least one action must be specified.
         /// </summary>
         /// <param name="ifSome">
-        ///     The action to execute if the <c>Optional</c> has a value
+        ///     The action to execute if the <c>Optional</c> has a value.
         /// </param>
         /// <param name="ifNone">
-        ///     The action to execute if the <c>Optional</c> has no value
+        ///     The action to execute if the <c>Optional</c> has no value.
         /// </param>
         /// <exception cref="ArgumentNullException">
         ///     If both <paramref name="ifSome"/> and <paramref name="ifNone"/>
-        ///     are null
+        ///     are null.
         /// </exception>
         public void Match(Action<T> ifSome = null, Action ifNone = null)
         {
@@ -103,7 +103,7 @@ namespace Alterna
         ///     <c>U</c>.
         /// </summary>
         /// <typeparam name="U">
-        ///     The destination type of the conversion
+        ///     The destination type of the conversion.
         /// </typeparam>
         /// <param name="ifSome">
         ///     A delegate that converts a value of the underlying type of the 
@@ -115,10 +115,10 @@ namespace Alterna
         ///     <c>Optional</c> has no value.
         /// </param>
         /// <returns>
-        ///     The result of the conversion
+        ///     The result of the conversion.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        ///     If <paramref name="ifSome"/> is <c>null</c>
+        ///     If <paramref name="ifSome"/> is <c>null</c>.
         /// </exception>
         public U Convert<U>(Func<T, U> ifSome, U ifNone)
         {
@@ -135,10 +135,10 @@ namespace Alterna
         ///     <c>Optional</c>.
         /// </summary>
         /// <typeparam name="U">
-        ///     The destination type of the mapping
+        ///     The destination type of the mapping.
         /// </typeparam>
         /// <param name="mapper">
-        ///     A mapping delegate to apply to the value, if present
+        ///     A mapping delegate to apply to the value, if present.
         /// </param>
         /// <returns>
         ///     An <c>Optional</c> describing the result of the mapping delegate
@@ -146,7 +146,7 @@ namespace Alterna
         ///     Otherwise an empty <c>Optional</c>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        ///     If <paramref name="mapper"/> is <c>null</c>
+        ///     If <paramref name="mapper"/> is <c>null</c>.
         /// </exception>
         public Optional<U> Map<U>(Func<T, U> mapper)
         {
@@ -165,17 +165,17 @@ namespace Alterna
         ///     Otherwise return an empty <c>Optional</c>.
         /// </summary>
         /// <typeparam name="U">
-        ///     The destination type of the mapping
+        ///     The destination type of the mapping.
         /// </typeparam>
         /// <param name="mapper">
-        ///     A mapping delegate to apply to the value, if present
+        ///     A mapping delegate to apply to the value, if present.
         /// </param>
         /// <returns>
         ///     An empty <c>Optional</c> describing the value of the result if
         ///     it has one. Otherwise an empty <c>Optional</c>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        ///     If <paramref name="mapper"/> is <c>null</c>
+        ///     If <paramref name="mapper"/> is <c>null</c>.
         /// </exception>
         public Optional<U> FlatMap<U>(Func<T, Optional<U>> mapper)
         {
@@ -191,7 +191,7 @@ namespace Alterna
         ///     value. Otherwise return an empty <c>Optional</c>.
         /// </summary>
         /// <param name="condition">
-        ///     A condition to test the value against
+        ///     A condition to test the value against.
         /// </param>
         /// <returns>
         ///     An <c>Optional</c> describing the value if it is present and 
@@ -199,7 +199,7 @@ namespace Alterna
         ///     <c>Optional</c>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        ///     If <paramref name="condition"/> is <c>null</c>
+        ///     If <paramref name="condition"/> is <c>null</c>.
         /// </exception>
         public Optional<T> When(Predicate<T> condition)
         {
@@ -219,7 +219,7 @@ namespace Alterna
         ///     the <c>Optional</c>.
         /// </summary>
         /// <param name="defaultValue">
-        ///     The value to return if the <c>Optional</c> has no value
+        ///     The value to return if the <c>Optional</c> has no value.
         /// </param>
         /// <returns>
         ///     The value of the <c>Optional</c> if it has one.
@@ -241,7 +241,7 @@ namespace Alterna
         /// </param>
         /// <returns>
         ///     <c>true</c> if the <c>Optional</c> has a value; otherwise,
-        ///     <c>false</c>
+        ///     <c>false</c>.
         /// </returns>
         public bool TryGetValue(out T value)
         {
@@ -254,16 +254,26 @@ namespace Alterna
         ///     Determines whether the specified object is equal to this object.
         /// </summary>
         /// <param name="other">
-        ///     The object to compare with the current object
+        ///     The object to compare with the current object.
         /// </param>
         /// <returns>
         ///     A bool value indicating whether the specified object is equal to
-        ///     this object
+        ///     this object.
         /// </returns>
         public bool Equals(Optional<T> other) =>
             (!HasValue && !other.HasValue)
             || (HasValue && other.HasValue && Value.Equals(other.Value));
 
+        /// <summary>
+        ///     Determines whether the specified object is equal to this object.
+        /// </summary>
+        /// <param name="obj">
+        ///     The object to compare with the current object.
+        /// </param>
+        /// <returns>
+        ///     A bool value indicating whether the specified object is equal to
+        ///     this object.
+        /// </returns>
         public override bool Equals(object obj)
         {
             if (obj == null && !HasValue)
@@ -278,16 +288,57 @@ namespace Alterna
             return false;
         }
 
+        /// <summary>
+        ///     Explicit conversion operator. Returns the value if present,
+        ///     otherwise throws an <see cref="InvalidOperationException"/>.
+        /// </summary>
+        /// <param name="v">
+        ///     The <c>Optional</c> to unwrap.
+        /// </param>
+        /// <returns>
+        ///     The value if present.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     If the <c>Optional</c> has no value.
+        /// </exception>
         public static explicit operator T(Optional<T> v) => v.Value;
 
+        /// <summary>
+        ///     Implicit conversion operator. Returns an <c>Optional</c>
+        ///     describing the left hand side.
+        /// </summary>
+        /// <param name="v">
+        ///     The value to wrap in an <c>Optional</c>.
+        /// </param>
+        /// <returns>
+        ///     An <c>Optional</c> describing the left hand side.
+        /// </returns>
         public static implicit operator Optional<T>(T v) =>
             v == null ? None : Some(v);
 
+        /// <summary>
+        ///     Returns the hash code of this <c>Optional</c>.
+        /// </summary>
+        /// <returns>
+        ///     The hash code of this <c>Optional</c>.
+        /// </returns>
         public override int GetHashCode() =>
             HasValue ? Value.GetHashCode() : 0;
 
-        public string DebuggerDisplay =>
+        /// <summary>
+        ///     Returns the string representation of this <c>Optional</c>.
+        /// </summary>
+        /// <returns>
+        ///     The string representation of this <c>Optional</c>.
+        /// </returns>
+        public override string ToString() =>
             HasValue ? "Some " + Value : "None";
+
+        /// <summary>
+        ///     Returns the string representation of this <c>Optional</c> to be
+        ///     used in the debugger.
+        /// </summary>
+        public string DebuggerDisplay => ToString();
 
         private Optional(T value)
         {
